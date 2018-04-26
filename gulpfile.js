0b736196ff964c150005f75cbf4a7e0b620743b4
Blazhkomikhail <michelin8585@gmail.com>
2018-04-26 23:00:33 +0300

    Create HTML-code

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function() {
+gulp.src('./src/**/Styles.less')
+      .pipe (less())
+      .pipe (gulp.dest('./dist/'));
+});
+
+gulp.task('less:watch', function() {
+	gulp.watch('./src/**/*.less', ['less']);
+});
