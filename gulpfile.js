c1fd0c493f64b39611b14f6e8dd9f50e99457e41
Blazhkomikhail <michelin8585@gmail.com>
2018-05-01 20:44:42 +0300

    Create Gender Button

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 gulp.task('less', function() {
-gulp.src('./src/**/Styles.less')
+gulp.src('./src/Styles.less')
       .pipe (less())
-      .pipe (gulp.dest('./dist/'));
+      .pipe (gulp.dest('./dist/CSS/'));
 });
 
 gulp.task('less:watch', function() {
