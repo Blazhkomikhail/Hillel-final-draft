f4f585fe19390efb73f6d81c9101b0cfa1b3da72
Blazhkomikhail <michelin8585@gmail.com>
2018-05-09 18:57:13 +0300

    Create Footer & Tab

--- dist/JS/JS.js
+++ dist/JS/JS.js
@@ -1,2 +1,22 @@
-$('.UserGate-Slick').slick();
+$(document).ready(function(){
 
+$(".UserList-tabItem").not(":first").hide();
+$(".UserList-tabWrap .UserList-tab").click(function() {
+	$(".UserList-tabWrap .UserList-tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".UserList-tabItem").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+$('.UserGate-Slick').slick({
+	dots: true,
+	autoplay: false,
+	arrows: false,
+	pauseOnHover: true,
+});
+
+$('.UserList-userSlider').slick({
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	infinite: true,
+});
+
+});
